f9e512ae021d3a620dbf57e909d27df9f2805aa1
Esanda <esanda617@gmail.com>
2025-09-07 11:08:14 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>My Web Page</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <body>
+        <h1>Welcome to My Web Page</h1>
+        <p>This is a sample web page with a linked CSS file.</p>
+    </body>
+</html>
